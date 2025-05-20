5f48b14162911f5d08572309f353c1f451e43a4e
faheelsattar <faheelsattar@yahoo.com>
2025-05-20 20:57:47 +0200

    test: select highest bid from slowest relay

--- server/service_test.go
+++ server/service_test.go
@@ -797,6 +797,66 @@ func TestGetHeaderBids(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, uint256.NewInt(12345), value)
 	})
+
+	t.Run("Use header with highest value with varying relay timeouts", func(t *testing.T) {
+		header := make(http.Header)
+		header.Set(HeaderAccept, MediaTypeJSON)
+
+		// Create backend and register 3 relays.
+		backend := newTestBackend(t, 3, time.Second)
+
+		// First relay will return signed response with value 12345.
+		backend.relays[0].GetHeaderResponse = backend.relays[0].MakeGetHeaderResponse(
+			12345,
+			"0xe28385e7bd68df656cd0042b74b69c3104b5356ed1f20eb69f1f925df47a3ab7",
+			"0xe28385e7bd68df656cd0042b74b69c3104b5356ed1f20eb69f1f925df47a3ab7",
+			"0x8a1d7b8dd64e0aafe7ea7b6c95065c9364cf99d38470c12ee807d55f7de1529ad29ce2c422e0b65e3d5a05c02caca249",
+			spec.DataVersionDeneb,
+		)
+		// with fastest respsonse
+		backend.relays[0].ResponseDelay = 100 * time.Millisecond
+
+		// Second relay will return signed response with value 12347.
+		backend.relays[1].GetHeaderResponse = backend.relays[1].MakeGetHeaderResponse(
+			12347,
+			"0xe28385e7bd68df656cd0042b74b69c3104b5356ed1f20eb69f1f925df47a3ab7",
+			"0xe28385e7bd68df656cd0042b74b69c3104b5356ed1f20eb69f1f925df47a3ab7",
+			"0x8a1d7b8dd64e0aafe7ea7b6c95065c9364cf99d38470c12ee807d55f7de1529ad29ce2c422e0b65e3d5a05c02caca249",
+			spec.DataVersionDeneb,
+		)
+		// with slowest respsonse
+		backend.relays[1].ResponseDelay = 300 * time.Millisecond
+
+		// First relay will return signed response with value 12346.
+		backend.relays[2].GetHeaderResponse = backend.relays[2].MakeGetHeaderResponse(
+			12346,
+			"0xe28385e7bd68df656cd0042b74b69c3104b5356ed1f20eb69f1f925df47a3ab7",
+			"0xe28385e7bd68df656cd0042b74b69c3104b5356ed1f20eb69f1f925df47a3ab7",
+			"0x8a1d7b8dd64e0aafe7ea7b6c95065c9364cf99d38470c12ee807d55f7de1529ad29ce2c422e0b65e3d5a05c02caca249",
+			spec.DataVersionDeneb,
+		)
+		// with medium paced respsonse
+		backend.relays[2].ResponseDelay = 200 * time.Millisecond
+
+		// Run the request.
+		rr := backend.request(t, http.MethodGet, path, header, nil)
+
+		// Each relay must have received the request.
+		require.Equal(t, 1, backend.relays[0].GetRequestCount(path))
+		require.Equal(t, 1, backend.relays[1].GetRequestCount(path))
+		require.Equal(t, 1, backend.relays[2].GetRequestCount(path))
+
+		require.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
+
+		// Highest value should be 12347, i.e. second relay even
+		// though its response time was the slowest.
+		resp := new(builderSpec.VersionedSignedBuilderBid)
+		err := json.Unmarshal(rr.Body.Bytes(), resp)
+		require.NoError(t, err)
+		value, err := resp.Value()
+		require.NoError(t, err)
+		require.Equal(t, uint256.NewInt(12347), value)
+	})
 }
 
 func TestGetPayload(t *testing.T) {
